a0778b6fa6e7d25f5b26e22bd71cc37f1f7af90d
amitabhdas96 <amitabh.sweden@gmail.com>
2024-02-24 13:41:00 +1300

    Commit on Feb 24th 2024

---
+++ npmPractice/index.js
@@ -0,0 +1,20 @@
+// objects in javascript contain keys (or properties) with corresponding values
+const book = {
+    
+    "description":"Srory of Life",
+    "author": "Amitabh Das",
+    "numberofpages": 500
+
+    }
+    
+    // we can access properties with dot notation
+    console.log(book.description);
+    console.log(book.author); 
+    console.log(book.numberofpages);
+    book.description = "Story of Death"
+    //console.log(user.age); // 25
+    // or with array style square bracket syntax
+    console.log(book); // Shark
+    //user.followers = 988; // we can also assign new values to object properties
+    //user.location = 'Pacific Ocean'; // or create new properties
+    
